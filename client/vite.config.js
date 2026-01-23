9f4032f93a04075c848570d30e6aa89b6eed40cd
Michael SCHAL <michaelschal@gmail.com>
2026-01-23 11:04:45 +0100

    Feat: Add Vite Proxy to route API/WS to backend internally (Simplifies deployment)

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,6 +7,19 @@ export default defineConfig({
     server: {
         host: true,
         port: 5173,
-        allowedHosts: ["snip.vonrodbox.eu"]
+        allowedHosts: ["snip.vonrodbox.eu"],
+        proxy: {
+            '/api': {
+                target: 'http://backend:4000',
+                changeOrigin: true,
+                secure: false
+            },
+            '/socket.io': {
+                target: 'http://backend:4000',
+                changeOrigin: true,
+                ws: true,
+                secure: false
+            }
+        }
     }
 })
